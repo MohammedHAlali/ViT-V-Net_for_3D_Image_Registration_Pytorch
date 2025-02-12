fd1701ba4ca8d93129113ec36fd169b3090032ce
Mohammed Alali <alali@login1.swan.hcc.unl.edu>
2025-02-12 02:13:36 -0600

    trying to run

--- ViT-V-Net/configs.py
+++ ViT-V-Net/configs.py
@@ -1,5 +1,28 @@
 import ml_collections
 
+def get_2DReg_config():
+    config = ml_collections.ConfigDict()
+    config.patches = ml_collections.ConfigDict({'size': (16, 16)})
+    config.patches.grid = (16, 16)
+    config.hidden_size = 512
+    config.transformer = ml_collections.ConfigDict()
+    config.transformer.mlp_dim = 2048 # 512 * 4
+    config.transformer.num_heads = 8 # 512 / 8
+    config.transformer.num_layers = 12 # can be reduced to 8
+    config.transformer.attention_dropout_rate = 0.0
+    config.transformer.dropout_rate = 0.1
+    config.patch_size = 16
+
+    config.conv_first_channel = 512
+    config.encoder_channels = (16, 32, 32)
+    config.down_factor = 2
+    config.down_num = 2
+    config.decoder_channels = (96, 48, 32, 32, 16)
+    config.skip_channels = (32, 32, 32, 32, 16)
+    config.n_dims = 2
+    config.n_skip = 5
+    return config
+
 def get_3DReg_config():
     config = ml_collections.ConfigDict()
     config.patches = ml_collections.ConfigDict({'size': (8, 8, 8)})
